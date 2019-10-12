5ce8a815214c4afa32135426829ba13fdd6a95db
Gopabandhu <gopabandhu@gopabandhus-macbook-air.local>
2019-10-12 19:53:41 +0530

    Initial Commit

---
+++ JsonParse/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JsonParse
+//
+//  Created by Gopabandhu on 12/10/19.
+//  Copyright © 2019 Gopabandhu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
